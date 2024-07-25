e49eaf6d4822e76d416846017cc72aebb9ce9dd6
victormc13 <viktorsb14@gmail.com>
2024-07-25 16:05:35 -0500

    new: Adds basic config file for neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,35 @@
+-- Set line numbers and relative numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Show matching brackets/parentheses
+vim.opt.showmatch = true
+
+-- Use spaces instead of tabs for indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Highlight current line
+vim.opt.cursorline = true
+
+-- Enable basic auto-indentation
+vim.opt.autoindent = true
+
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*',
+    command = ':%s/\\s\\+$//e',
+})
+
+-- Improve search highlighting
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Set colorscheme and syntax color
+vim.cmd('syntax on')
+vim.cmd('colorscheme wildcharm')
+vim.cmd('hi Normal ctermbg=NONE')
+
+-- Enables mouse usage
+vim.opt.mouse = 'a'
